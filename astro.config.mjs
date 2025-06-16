b11355b8d7c72c1174586ddcbb5f26a950563352
Tyler Evans <snaveevans@users.noreply.github.com>
2025-06-15 20:33:40 -0600

    Use astro (#1)
    * switch to astro
    
    * update about me
    
    * format everything
    
    * update home page & site title
    
    * add cloudflare adapter
    
    * re-add wrangler.toml
    
    * merge changes from cloudflare template
    
    * fix app name

---
+++ astro.config.mjs
@@ -0,0 +1,19 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+
+import cloudflare from '@astrojs/cloudflare';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://www.tylerevans.co',
+  integrations: [mdx(), sitemap()],
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true
+    },
+
+    imageService: "cloudflare"
+  }),
+});
